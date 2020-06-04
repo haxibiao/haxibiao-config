763b9a193d59327d1a248d3acfd7fcc2df3ec9c5
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-06-04 14:26:24 +0800

    兼容答赚web

--- src/helpers/aso.php
+++ src/helpers/aso.php
@@ -3,53 +3,6 @@
 use haxibiao\config\AppConfig;
 use haxibiao\config\Aso;
 use haxibiao\config\Config;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-
-function small_logo(): string
-{
-    if (project_is_dtzq()) {
-        return '/picture/logo.png';
-    }
-    $logo = Aso::getValue('下载页', 'logo') ?? '';
-    if (empty($logo)) {
-        return '/logo/' . env('APP_DOMAIN') . '.small.png';
-    } else {
-        return $logo;
-    }
-}
-
-//兼容网页
-function qrcode_url()
-{
-
-    if (project_is_dtzq()) {
-        $apkUrl = "http://datizhuanqian.com/download"; //TODO: env?
-    } else {
-        $apkUrl = Aso::getValue('下载页', '安卓地址');
-    }
-    $logo   = small_logo();
-    $qrcode = QrCode::format('png')->size(250)->encoding('UTF-8');
-    //要合并生成带网站logo的二维码，logo必须放本地
-    if (file_exists(public_path($logo))) {
-        $qrcode->merge(public_path($logo), .1, true);
-    }
-    $qrcode = $qrcode->generate($apkUrl);
-    return base64_encode($qrcode);
-}
-
-//检查是否备案期间
-function isRecording()
-{
-
-    $config = AppConfig::where([
-        'group' => 'record',
-        'name'  => 'web',
-    ])->first();
-    if ($config && $config->state === AppConfig::STATUS_ON) {
-        return true;
-    }
-    return false;
-}
 
 function adIsOpened()
 {
@@ -94,4 +47,63 @@ function douyinOpen()
         return true;
     }
 
+}
+
+/*****************************
+ * *****答赚web网页兼容*********
+ * ***************************
+ */
+//是否处于备案状态
+function isRecording()
+{
+    if (class_exists("App\\AppConfig", true)) {
+
+        $config = \App\AppConfig::where([
+            'group' => 'record',
+            'name'  => 'web',
+        ])->first();
+        if ($config === null) {
+            return true;
+        }
+        if ($config->state === \App\AppConfig::STATUS_ON) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+function small_logo()
+{
+    if (class_exists("App\\Aso", true)) {
+        $logo = \App\Aso::getValue('下载页', 'logo');
+
+        if (empty($logo)) {
+            return '/logo/' . env('APP_DOMAIN') . '.small.png';
+        } else {
+            return $logo;
+        }
+    }
+}
+
+function qrcode_url()
+{
+    if (class_exists("App\\Aso", true)) {
+        $apkUrl = \App\Aso::getValue('下载页', '安卓地址');
+        $logo   = small_logo();
+        $qrcode = SimpleSoftwareIO\QrCode\Facades\QrCode::format('png')->size(250)->encoding('UTF-8');
+        if (str_contains($logo, env('COS_DOMAIN'))) {
+            $qrcode->merge($logo, .1, true);
+        } else {
+            if (file_exists(public_path($logo))) {
+                $qrcode->merge(public_path($logo), .1, true);
+            }
+        }
+
+        $qrcode = $qrcode->generate($apkUrl);
+
+        $path = base64_encode($qrcode);
+
+        return $path;
+    }
 }
--- src/helpers/seo.php
+++ src/helpers/seo.php
@@ -7,12 +7,6 @@ function seo_value($group, $name)
     return Seo::getValue($group, $name);
 }
 
-function get_seo_meta()
-{
-    return Seo::getValue('百度', 'meta');
-
-}
-
 function get_seo_push()
 {
     return Seo::getValue('百度', 'push');
@@ -23,17 +17,33 @@ function get_seo_tj()
     return Seo::getValue('统计', 'matomo');
 }
 
+/*****************************
+ * *****答赚web网页兼容*********
+ * ***************************
+ */
 function get_seo_title()
 {
-    return Seo::getValue('TKD', 'title');
+    return \App\Seo::getValue('TKD', 'title');
 }
 
 function get_seo_keywords()
 {
-    return Seo::getValue('TKD', 'keywords');
+    return \App\Seo::getValue('TKD', 'keywords');
 }
 
 function get_seo_description()
 {
-    return Seo::getValue('TKD', 'description');
+    return \App\Seo::getValue('TKD', 'description');
+}
+
+function get_seo_meta()
+{
+    // $meta = '';
+    // if (Storage::exists("seo_config")) {
+    //     $json   = Storage::get('seo_config');
+    //     $config = json_decode($json);
+    //     $meta   = $config->seo_meta;
+    // }
+    // return $meta;
+    return \App\Seo::getValue('百度', 'meta');
 }
