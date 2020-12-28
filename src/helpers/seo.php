bb03f4c47cccd0033bf3bdb515becc2bb0414380
张志明 <zhangzhiming@haxifang.com>
2020-12-28 15:30:03 +0800

    feat：修改电影权重的时候自动push到百度api

--- src/helpers/seo.php
+++ src/helpers/seo.php
@@ -3,6 +3,23 @@
 use Haxibiao\Config\Facades\SEOFriendlyUrl;
 use Haxibiao\Config\Seo;
 
+function push_url_baidu($urls)
+{
+    $urls = array($urls);
+    $api = 'http://data.zz.baidu.com/urls?site=https://neihandianying.com&token=0Oay0vFOYGDk9x34';
+    $ch = curl_init();
+    $options =  array(
+        CURLOPT_URL => $api,
+        CURLOPT_POST => true,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POSTFIELDS => implode("\n", $urls),
+        CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+    );
+    curl_setopt_array($ch, $options);
+    $result = curl_exec($ch);
+    echo  $result;
+}
+
 function seo_friendly_urls()
 {
     return SEOFriendlyUrl::generate();
